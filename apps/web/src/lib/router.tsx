56a78c53d88dc6ed26bb70521488e2ca117cc253
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2026-08-24 17:30:32 -0300

    feat(web): add source onboarding page

--- apps/web/src/lib/router.tsx
+++ apps/web/src/lib/router.tsx
@@ -1,9 +1,35 @@
-import { createRootRoute, createRouter } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet,
+} from "@tanstack/react-router";
 
 import { LandingPage } from "../pages";
+import { OnboardingPage } from "../pages/onboarding";
 
-const rootRoute = createRootRoute({ component: LandingPage });
+const rootRoute = createRootRoute({ component: Outlet });
+const landingRoute = createRoute({
+  component: LandingPage,
+  getParentRoute: () => rootRoute,
+  path: "/",
+});
+const appRoute = createRoute({
+  component: Outlet,
+  getParentRoute: () => rootRoute,
+  path: "app",
+});
+const onboardingRoute = createRoute({
+  component: OnboardingPage,
+  getParentRoute: () => appRoute,
+  path: "onboarding",
+});
+
+const routeTree = rootRoute.addChildren([
+  landingRoute,
+  appRoute.addChildren([onboardingRoute]),
+]);
 
 export const router = createRouter({
-  routeTree: rootRoute,
+  routeTree,
 });
